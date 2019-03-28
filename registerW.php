ed3238f4e70cd52088bed190509b233f3fe7c281
cindywe <33615692+cindywe@users.noreply.github.com>
2019-03-27 18:00:20 -0700

    Add files via upload

--- registerW.php
+++ registerW.php
@@ -13,7 +13,7 @@ if ($user != "") {
 }
 ?>
 </div>
-<form action="register.php" method="post">
+<form action="registerW.php" method="post">
  <br>
  <br>
  <label>Username:</label>
@@ -23,6 +23,10 @@ if ($user != "") {
  <label>Password:</label>
  <input name="password" type="password" placeholder="Type Here">
  <br>
+ <label>Name:</label>
+ <br>
+ <input name="name" type="text" placeholder="Type Here">
+ <br>
  <br>
  <label>Image:</label>
  <b>Select a file</b>
@@ -31,11 +35,14 @@ if ($user != "") {
  <label>Phone:</label>
  <input name="phoneNum" type="text" placeholder="Type Here">
  <br>
- <label>Are u a Walker or Owner</label>
+ <label>personalBio:</label>
+ <br>
+ <input name="personalBio" type="text" placeholder="Type Here">
+ <!-- <label>Are u a Walker or Owner</label>
  <select name="walkerorowner">
    <option value="dogwalker">Walker</option>
   <option value="dogowner">Owner</option>
- </select>
+ </select> -->
  <br>
  <br><br>
  <input type="submit" name="registration" value="Register">
@@ -48,13 +55,34 @@ $conn = OpenCon();
 if (array_key_exists('registration', $_POST)) {
     $username = $_POST['username'];
     $password = $_POST['password'];
+    $personalBio = $_POST['personalBio'];
     $userImage = $_POST['userImage'];
     $phoneNum = $_POST['phoneNum'];
-    $walkerorowner = $_POST['walkerorowner'];
-    if (!$username || !$password || !$userImage || !$phoneNum || !$walkerorowner) {
+    $name = $_POST['name'];
+    //$walkerorowner = $_POST['walkerorowner'];
+    if (!$username || !$password || !$userImage || !$phoneNum|| !$personalBio||!$name) {
         echo "Please fill out all inputs.";
     }
     else {
+      $sql = "insert into walkerNameNum values('$phoneNum','$name')";
+      $result = $conn->query($sql);
+      if ($result === TRUE) {
+        $sql = "insert into dogwalker values ('$username', '$password', '$userImage', '$phoneNum','$personalBio',0)";
+        $result = $conn->query($sql);
+        if ($result === TRUE) {
+            echo "<div>Account with username = $username created successfully!</div><br>";
+            echo "<form action='viewaccount.php'>
+        <button type='submit'>View Account</button>
+       </form>";
+        }
+        else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+      }
+      else {
+        echo "Error: ". $sql ."<br>".$conn->error;
+      }
       // to aviod duplicate username
       // $sql = "select username from '$walkerorowner'"
       // $result = $conn->query($sql);
@@ -69,17 +97,7 @@ if (array_key_exists('registration', $_POST)) {
       // not sure what to do with the image!!!
         //
         //$user = $_SESSION["user"];
-        $sql = "insert into $walkerorowner values ('$username', '$password', '$userImage', '$phoneNum')";
-        $result = $conn->query($sql);
-        if ($result === TRUE) {
-            echo "<div>Account with username = $username created successfully!</div><br>";
-            echo "<form action='viewaccount.php'>
-        <button type='submit'>View Account</button>
-       </form>";
-        }
-        else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
-        }
+
     }
 }
 CloseCon($conn);
